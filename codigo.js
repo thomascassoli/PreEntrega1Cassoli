431bdf07d7eebebeca7bc6a9729e71c1b348d580
thomas cassoli <thomascassoli21@gmail.com>
2023-07-23 21:11:27 -1200

    subo mi proyecto de venta de autos

--- codigo.js
+++ codigo.js
@@ -1,26 +1,57 @@
 let usuario = "";
-let nombreUsuario = prompt("Ingrese su nombre de usuario");
+let nombreUsuario = prompt("Ingrese su nombre y apellido");
  
 alert("Bienvenido/a " + nombreUsuario); 
 
-let edad = prompt("Solo podes solicitar un turno si sos mayor de edad. Por favor ingrese su edad:");
+let edad = prompt("Solo podes solicitar ingresar si es mayor de edad. Por favor ingrese su edad:");
 if (edad >= 18){
-    alert("Sos mayor de edad. Podes solicitar tu turno.");
+    alert("Sos mayor de edad. Podes solicitar lista de vehiculos.");
 }else {
-        alert("Sos menor de edad. No podes solicitar tu turno.");
+        alert("Sos menor de edad. No podes solicitar la lista.");
     }
 
-let dia1 = "lunes";
-let dia2 = "martes";
+console.log("Tengo estos vehiculos a la venta, a continuacion te muestro caules son..");
 
-let diaDevacunacion = prompt("Dias disponibles para turno de vacunacion, Lunes y Martes. Por favor escriba el dia que pueda asistir y le brindaremos el horario.");
+const vehiculos = [
+   { auto: "volkswagen gol trend", precio: 3000000},
+   { auto: "chevrolet vectra", precio: 2100000},
+   { auto: "ford fiesta", precio: 200000},
+   { auto: "fiat uno", precio: 900000}
+]
 
-if (diaDevacunacion == "lunes"){
-    alert ("Su turno es el dia " + diaDevacunacion  + " a las 18:15 pm");
-}else if (diaDevacunacion == "martes"){
-    alert ("Su turno es el dia " + diaDevacunacion  + " a las 10:20 am");
-}else{
-    alert("Dia seleccionado incorrecto");
+vehiculos.forEach(auto => {
+    console.log (auto.auto);
+    
+});
+
+prompt("Te interesa saber mas sobre los vehiculos?");
+
+function Autos(marca, color, precio){
+
+    this.marca = marca;
+    this.color = color;
+    this.precio = precio;
 }
 
-let finalizacion = alert("Gracias por solicitar su turno.");
+const Autos1 = new Autos ("Volkswagen gol trend", "negro", "3.000.000");
+const Autos2 = new Autos("Chevrolet vectra", "gris", "2.100.000");
+const Autos3 = new Autos("Ford fiesta", "rojo", "2.00.000");
+const Autos4 = new Autos("Fiat uno", "blanco", "900.000");
+
+console.log(Autos1);
+console.log(Autos2);
+console.log(Autos3);
+console.log(Autos4);
+
+prompt("Poner si para saber cual es el auto mas barato al mas caro");
+
+const autos = [' Volkswagen' , ' Chevrolet' , ' Ford' , ' Fiat' ];
+autos.reverse();
+alert(autos);
+
+
+let autoAcomprar = prompt("indique el auto a que quiera comprar..");
+console.log(vehiculos.find((Auto)=> Auto.auto === autoAcomprar))
+
+
+let finalizacion = alert("Gracias. Cualquier consulta, estamos a su dispocision.");
